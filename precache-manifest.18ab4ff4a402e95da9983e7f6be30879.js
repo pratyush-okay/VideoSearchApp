e3b4be92a2da3f5d4e5a3479028a513f1025afda
pratyush <pratyushpanda555@gmail.com>
2020-10-27 01:57:31 +0530

    Updates

---
+++ precache-manifest.18ab4ff4a402e95da9983e7f6be30879.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea43ffbe024f3a0882a95e20e0bf3b76",
+    "url": "/VideoSearchApp/index.html"
+  },
+  {
+    "revision": "9a976bd6b619bd7503bd",
+    "url": "/VideoSearchApp/static/css/2.184c70ed.chunk.css"
+  },
+  {
+    "revision": "eec3852af47f7d3f4467",
+    "url": "/VideoSearchApp/static/css/main.a5b5cb57.chunk.css"
+  },
+  {
+    "revision": "9a976bd6b619bd7503bd",
+    "url": "/VideoSearchApp/static/js/2.231d6821.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/VideoSearchApp/static/js/2.231d6821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec3852af47f7d3f4467",
+    "url": "/VideoSearchApp/static/js/main.019b90d9.chunk.js"
+  },
+  {
+    "revision": "6da89506bff3a48da4ea",
+    "url": "/VideoSearchApp/static/js/runtime-main.2f38ffa6.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/VideoSearchApp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/VideoSearchApp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/VideoSearchApp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/VideoSearchApp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/VideoSearchApp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/VideoSearchApp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/VideoSearchApp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/VideoSearchApp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/VideoSearchApp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/VideoSearchApp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/VideoSearchApp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/VideoSearchApp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/VideoSearchApp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/VideoSearchApp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/VideoSearchApp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/VideoSearchApp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
